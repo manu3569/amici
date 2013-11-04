e4e1e5c7159f75edea1ce3d484ec5528cb2b8f74
manu3569 <manuel.neuhauser@flatironschool.com>
2013-11-04 14:55:26 -0500

    Moved logger setup to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,13 @@ class ApplicationController < Sinatra::Base
 
   configure :development do
     register Sinatra::Reloader
+    Logger.new("log/amici.log")
+    enable :logging, :dump_errors
+    set :raise_errors, true
   end
 
   enable  :sessions, :logging, :dump_errors
-  
+
   set :views, Proc.new { File.join(root, "../views/") }
 
   def self.get_or_post(url, &block)
--- config.ru
+++ config.ru
@@ -1,8 +1,5 @@
 require './config/environment.rb'
 
-log = File.new("log/amici.log", "a")
-STDOUT.reopen(log)
-STDERR.reopen(log)
 
 use HooksController
 run ApplicationController
