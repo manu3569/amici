ac002bf57e53ee65edf18794dfe05a1f3c866d4d
manu3569 <manuel.neuhauser@flatironschool.com>
2013-11-04 15:04:40 -0500

    Added friendly response.

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -7,6 +7,7 @@ class HooksController < ApplicationController
 
   get_or_post "/" do
     File.open("log/amici.log", 'a') { |file| file.write("Request received at: #{Time.now}: \n" + request.inspect) }
+    "Thanks, GitHub! :D"
   end
 
 end
