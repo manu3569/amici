75f05572dc8a6803e146173d295289db18c3c3de
manu3569 <manuel.neuhauser@flatironschool.com>
2013-11-06 07:40:24 -0500

    Fixed bug in Student model.

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,7 +9,7 @@ class Student < Sequel::Model
 
     uri = URI("https://api.github.com")
     Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-      request = Net::HTTP::Get.new("/user?access_token=#{self.token}")
+      request = Net::HTTP::Get.new("/user?access_token=#{token}")
       response = http.request(request)
       info = JSON.parse(response.body)
     end
